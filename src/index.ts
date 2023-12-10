a4ffdb5643391f52fd242699d7bb961cc2ebb95a
Meng Zhou <Meng.Zhou@aer.ca>
2023-12-10 09:17:05 -0700

    commit

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response } from 'express'
 
-const app = express();
+const app = express()
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, World');
-});
+  res.send('Hello, World version 1')
+})
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 
 app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
+  console.log(`Server is listening on port ${port}`)
 })

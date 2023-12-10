1c3513a934582e5c15343a8e14d00f461451021a
Meng Zhou <Meng.Zhou@aer.ca>
2023-12-10 09:21:28 -0700

    commit

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import express, { Request, Response } from 'express'
 const app = express()
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, World version 1')
+  res.send('Hello, World version 2')
 })
 
 const port = process.env.PORT || 3000
